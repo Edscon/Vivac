4ede2b46dbce77b827c0cf9c60839303368efdfb
Edscon <eduard.soler.11@gmail.com>
2023-03-30 21:06:11 +0200

    first commit

--- vivac/settings.py
+++ vivac/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
