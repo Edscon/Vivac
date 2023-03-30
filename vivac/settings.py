f408cb39534db398d26dd80f804161e03200b149
Edscon <eduard.soler.11@gmail.com>
2023-03-30 22:51:50 +0200

    first commit

--- vivac/settings.py
+++ vivac/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
