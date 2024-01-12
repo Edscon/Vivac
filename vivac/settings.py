274d7740475ee95b484da2a15c57dee0a1799f3a
Edscon <eduard.soler.11@gmail.com>
2024-01-12 11:44:47 +0100

    Commit

--- vivac/settings.py
+++ vivac/settings.py
@@ -85,21 +85,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'django_cleanup',
-
-    'tailwind',
-    'theme',
-    'django_browser_reload',
 ]
-
-TAILWIND_APP_NAME = 'theme'
-
-INTERNAL_IPS = [
-
-    "127.0.0.1",
-
-]
-
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
@@ -121,8 +107,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'vivac.urls'
