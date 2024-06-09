9af815f26625ac61d495393403e523e7f2bede4b
Edscon <eduard.soler.11@gmail.com>
2024-06-09 22:02:07 +0200

    commit

--- vivac/settings.py
+++ vivac/settings.py
@@ -56,16 +56,16 @@ STRIPE_WEBHOOK_SECRET_TEST = os.getenv('STRIPE_WEBHOOK_SECRET_TEST')
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+'''
 SITE_ID = 2
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''
+
 SITE_ID = 3
 DATABASES = {
     'default': {
@@ -79,7 +79,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
-}'''
+}
 
 
 # CKEditor Settings
