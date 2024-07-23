39711aad82ab860b9da2fae8aef738aea23fd68c
Edscon <eduard.soler.11@gmail.com>
2024-07-23 16:33:48 +0200

    Media Files Correct Configuration

--- vivac/urls.py
+++ vivac/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
@@ -12,9 +15,10 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('accounts/', include('allauth.urls')),
     path('dashboard/', include('dashboard.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
-
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 '''urlpatterns += i18n_patterns(
